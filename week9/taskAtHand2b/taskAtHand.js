26b9fb29da11e767ab8e76210a5a5da6aa8b1d5d
CoderJoesFreelanceDevs <31904496+CoderJoesFreelanceDevs@users.noreply.github.com>
2017-11-06 15:30:12 +0800

    2b test

---
+++ week9/taskAtHand2b/taskAtHand.js
@@ -0,0 +1,179 @@
+"use strict";
+//NOTE: I followed the book's guide to this lab so my code looks similar, but I took the time to understand what I was doing, and explored alternative syntax and logic (that usually didn't work)
+// using a function contructor form to create an object
+function TaskAtHandApp()
+{
+	var version = "v1.0",
+	appStorage = new AppStorage("taskAtHand");
+
+	// creating a private function
+	function setStatus(message)
+	{
+		$("#app>footer").text(message);
+	}
+
+	// creating a public function
+	this.start = function()
+	{
+		$("#app>header").append(version);
+		loadTaskList();
+		setStatus("ready");
+				
+		$("#newtask").keypress(function(e) {
+		if (e.which == 13) 
+		{
+			//alert("keypress is detected!");
+			newTask();
+			return false;
+		}
+		$("#theme").change(onChangeTheme);
+	})	.focus();
+	};
+	
+	function newTask()
+	{
+		var task = $("#taskentry").val();
+		//alert();
+		if(task)
+		{
+			//alert("newTask isnt null");
+			addToList(task);
+			$("#newTask").val("").focus();
+		}
+		saveTaskList();
+	}
+	
+	function addToList(taskName)
+	{
+		//Note: old function was replaced to incorporate template
+		var $task = $("#task-template .task").clone();
+		$("span.task-name", $task).text(taskName);
+		$("#task-list").append($task);
+		$task.click(function() { onSelectTask($task); });
+		$("button.delete", $task).click(function() {
+			deleteTask($task);
+		});
+		$("button.move-up", $task).click(function() {
+			moveUpTask($task);
+		});
+		$("button.move-down", $task).click(function() {
+			moveDownTask($task);
+		});
+		$("span.task-name", $task).click(function() {
+			editTask($(this));
+		});
+		$("input.task-name", $task).change(function() {
+		changeTaskName($(this));
+		});
+		$("span.task-name", $task).blur(function() {
+		$(this).hide().siblings("span.task-name").show();
+		});
+	}
+	
+	function deleteTask($task)
+	{
+		$task.remove();
+		saveTaskList();
+	}
+	
+	function moveUpTask($task)
+	{
+		$task.insertBefore($task.prev());
+		saveTaskList();
+	}
+	
+	function moveDownTask($task)
+	{
+		$task.insertAfter($task.next());
+		saveTaskList();
+	}
+	
+	function editTask($name)
+	{
+		$name.hide()
+			.siblings("input.task-name")
+			.val($name.text())
+			.show()
+			.focus();
+		saveTaskList();
+	}
+	
+	function changeTaskName($input)
+	{
+		$input.hide();
+		var $span = $input.siblings("span.task-name");
+		if ($input.val())
+		{
+			$span.text($input.val());
+		}
+		$span.show();
+		saveTaskList();
+	}
+	
+	function onChangeTheme()
+	{
+		var theme = $("#theme>option").filter(":selected").val();
+		setTheme(theme);
+		appStorage.setValue("theme", theme);
+	}
+	
+	function onSelectTask($task)
+	{
+		if ($task)
+		{
+			// Unselect other tasks
+			$task.siblings(".selected").removeClass("selected");
+			// Select this task
+			$task.addClass("selected");
+		}
+	}
+	
+	function setTheme(theme)
+	{
+		$("#theme-style").attr("href", "themes/" + theme + ".css");
+	}
+
+	var theme = appStorage.getValue("theme");
+	function loadTheme()
+	{
+		if (theme)
+		{
+		setTheme(theme);
+		$("#theme>option[value=" + theme + "]")
+		.attr("selected","selected");
+		}
+	}
+	function saveTaskList()
+	{
+		var tasks = [];
+		$("#task-list .task span.task-name").each(function() {
+		tasks.push($(this).text());
+		});
+		appStorage.setValue("taskList", tasks);
+		//alert(tasks);
+	}
+	
+	function loadTaskList()
+	{
+		var tasks = appStorage.getValue("taskList");
+		if (tasks)
+		{
+			for (var i in tasks)
+			{
+				addToList(tasks[i]);
+			}
+		}
+	}
+		// end MyApp
+}
+/* 	JQuery's shorthand for the document ready event handler
+		could be written: $(document).ready(handler);
+
+		When this page loads, we'll create a global variable
+		named "app" by attaching it to the "window" object
+		(part of the BOM - Browser Object Model)
+*/
+$(function() {
+	window.taskAtHand = new TaskAtHandApp();
+	window.taskAtHand.start();
+});
